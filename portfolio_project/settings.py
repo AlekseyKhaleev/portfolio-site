d38cd129cf6d421c5c2904be38758ac732dce0da
AlekseyKhaleev <aleksey52nn@gmail.com>
2022-04-12 14:15:19 +0300

    remove trash

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -105,11 +105,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
+
+DATE_FORMAT = 'd E Y'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
+USE_L10N = True
 
 USE_TZ = True
 
